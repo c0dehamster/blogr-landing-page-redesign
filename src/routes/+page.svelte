3664941a723ab1dab70159b6c5298a17882ec745
Sergej Smelov <109071165+c0dehamster@users.noreply.github.com>
2023-05-22 17:53:46 +0500

    Layout for the hero screen (mobile)

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,7 @@
-<script>
+<script lang="ts">
 	import illustration from "../lib/images/Illustration.png"
+
+	import CardList from "./CardList.svelte"
 </script>
 
 <main class="main">
@@ -20,66 +22,46 @@
 		</div>
 	</section>
 
-	<section class="services">
-		<div class="services__divider">
-			<h2 class="services__heading">Designed for the future</h2>
-		</div>
-
-		<ul class="services__list">
-			<li class="services__list-item">
-				<div class="card">
-					<h3 class="card__heading">
-						Introducing an extensible editor
-					</h3>
-					<p class="card__description">
-						Blogr features an exceedingly intuitive interface which
-						lets you focus on one thing: creating content. The
-						editor supports management of multiple blogs and allows
-						easy manipulation of embeds such as images, videos, and
-						Markdown. Extensibility with plugins and themes provide
-						easy ways to add functionality or change the looks of a
-						blog.
-					</p>
-				</div>
-			</li>
-
-			<li class="services__list-item">
-				<div class="card">
-					<h3 class="card__heading">Robust content management</h3>
-					<p class="card__description">
-						Flexible content management enables users to easily move
-						through posts. Increase the usability of your blog by
-						adding customized categories, sections, format, or flow.
-						With this functionality, you’re in full control.
-					</p>
-				</div>
-			</li>
-
-			<li class="services__list-item">
-				<div class="card">
-					<h3 class="card__heading">Free, open, simple</h3>
-					<p class="card__description">
-						Blogr is a free and open source application backed by a
-						large community of helpful developers. It supports
-						features such as code syntax highlighting, RSS feeds,
-						social media integration, third-party commenting tools,
-						and works seamlessly with Google Analytics. The
-						architecture is clean and is relatively easy to learn.
-					</p>
-				</div>
-			</li>
-
-			<li class="services__list-item">
-				<div class="card">
-					<h3 class="card__heading">Powerful tooling</h3>
-					<p class="card__description">
-						Batteries included. We built a simple and
-						straightforward CLI tool that makes customization and
-						deployment a breeze, but capable of producing even the
-						most complicated sites.
-					</p>
-				</div>
-			</li>
-		</ul>
-	</section>
+	<CardList />
 </main>
+
+<style>
+	.hero-screen {
+		padding-block: 7rem;
+		padding-inline: var(--padding-main-responsive);
+
+		display: grid;
+		gap: 4rem;
+		justify-items: center;
+	}
+
+	.heading {
+		font-size: var(--font-size-700);
+		text-align: center;
+	}
+
+	.hero {
+		display: grid;
+		gap: 4rem;
+	}
+
+	.hero__contents {
+		max-width: 25rem;
+		justify-self: center;
+		display: grid;
+		gap: 1.5rem;
+		grid-template-areas:
+			"description description"
+			"start-for-free learn-more";
+	}
+
+	.hero__description {
+		max-width: 20rem;
+
+		grid-area: description;
+		justify-self: center;
+
+		text-align: center;
+		font-size: var(--font-size-400);
+	}
+</style>
---
+++ src/routes/CardList.svelte
@@ -0,0 +1,58 @@
+<section class="services">
+	<div class="services__divider">
+		<h2 class="services__heading">Designed for the future</h2>
+	</div>
+
+	<ul class="services__list">
+		<li class="services__list-item">
+			<div class="card">
+				<h3 class="card__heading">Introducing an extensible editor</h3>
+				<p class="card__description">
+					Blogr features an exceedingly intuitive interface which lets
+					you focus on one thing: creating content. The editor
+					supports management of multiple blogs and allows easy
+					manipulation of embeds such as images, videos, and Markdown.
+					Extensibility with plugins and themes provide easy ways to
+					add functionality or change the looks of a blog.
+				</p>
+			</div>
+		</li>
+
+		<li class="services__list-item">
+			<div class="card">
+				<h3 class="card__heading">Robust content management</h3>
+				<p class="card__description">
+					Flexible content management enables users to easily move
+					through posts. Increase the usability of your blog by adding
+					customized categories, sections, format, or flow. With this
+					functionality, you’re in full control.
+				</p>
+			</div>
+		</li>
+
+		<li class="services__list-item">
+			<div class="card">
+				<h3 class="card__heading">Free, open, simple</h3>
+				<p class="card__description">
+					Blogr is a free and open source application backed by a
+					large community of helpful developers. It supports features
+					such as code syntax highlighting, RSS feeds, social media
+					integration, third-party commenting tools, and works
+					seamlessly with Google Analytics. The architecture is clean
+					and is relatively easy to learn.
+				</p>
+			</div>
+		</li>
+
+		<li class="services__list-item">
+			<div class="card">
+				<h3 class="card__heading">Powerful tooling</h3>
+				<p class="card__description">
+					Batteries included. We built a simple and straightforward
+					CLI tool that makes customization and deployment a breeze,
+					but capable of producing even the most complicated sites.
+				</p>
+			</div>
+		</li>
+	</ul>
+</section>
