c685cc895c22d13780add9ce53420472da5e33d1
Sergej Smelov <109071165+c0dehamster@users.noreply.github.com>
2023-05-27 22:31:57 +0500

    Hero section for tablet (needs refactoring)

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,13 +10,15 @@
 		<h1 class="heading">A modern publishing platform</h1>
 
 		<div class="hero">
-			<div class="hero__contents">
-				<p class="hero__description">
-					Grow your audience and build your online brand
-				</p>
+			<div class="hero__contents-wrapper">
+				<div class="hero__contents">
+					<p class="hero__description">
+						Grow your audience and build your online brand
+					</p>
 
-				<button class="button button--solid">Start for Free</button>
-				<button class="button button--outline">Learn More</button>
+					<button class="button button--solid">Start for Free</button>
+					<button class="button button--outline">Learn More</button>
+				</div>
 			</div>
 
 			<img src={illustration} alt="" class="hero__image" />
@@ -55,9 +57,11 @@
 		gap: 4rem;
 	}
 
-	.hero__contents {
-		max-width: 25rem;
+	.hero__contents-wrapper {
 		justify-self: center;
+	}
+
+	.hero__contents {
 		display: grid;
 		gap: 1.5rem;
 		grid-template-areas:
@@ -75,5 +79,45 @@
 
 		text-align: center;
 		font-size: var(--font-size-400);
+	}
+
+	@media screen and (min-width: 40rem) {
+		.hero {
+			/* Overlap content with grid */
+
+			grid-template-rows: repeat(7, 1fr);
+			grid-template-columns: repeat(6, 1fr);
+			gap: 0px;
+		}
+
+		.hero__contents-wrapper,
+		.hero__image {
+			grid-row: 4 / 5;
+		}
+
+		.hero__contents-wrapper {
+			width: 100%;
+
+			padding-inline: 3.5rem;
+			padding-block: 10% 15%;
+
+			grid-row: 1 / 5;
+			grid-column: 1 / 6;
+
+			border: 2px solid var(--color-neutral-400);
+		}
+
+		.hero__contents {
+			gap: 2.5rem;
+		}
+
+		.hero__image {
+			grid-column: 2/ -1;
+			grid-row: 4 / -1;
+		}
+
+		.hero__description {
+			font-size: var(--font-size-500);
+		}
 	}
 </style>
