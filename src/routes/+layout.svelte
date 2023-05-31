c04827203d664e0ce23db9e1fe2e3add44d3991f
Sergej Smelov <109071165+c0dehamster@users.noreply.github.com>
2023-05-31 11:04:24 +0500

    Add title

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,6 +5,10 @@
 	import Footer from "./Footer.svelte"
 </script>
 
+<svelte:head>
+	<title>Frontend Mentor | Blogr landing page</title>
+</svelte:head>
+
 <div class="app">
 	<HeaderNew />
 	<slot />
